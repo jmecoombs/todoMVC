f9a1fbcb20f3b8924b25ede8768d53f8037efa57
Jamie Coombs <jamiec.coombs@gmail.com>
2013-10-05 11:03:25 -0600

    experimenting w/adding to do with an 'add button'
    having issues with typing in text but it's not adding to the list below
    after clicking 'add' button.

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
-var listItems = function (text) {
+/*
+ * var listItems = function (text) {
  var result = [];
     for(var index = 0; index < text.length; index++) {
 
@@ -10,3 +11,27 @@ var listItems = function (text) {
     document.write('<li>' + text[i] + '</li>');
     return result;
 }
+*/ 
+
+$(document).ready(function() {
+ 
+    $('#add_todo').click( function() {
+        var todoDescription = $('#todo_description').val();
+        $('.todo_list').prepend('<div class='todo'>'
+                    + '<div>'
+                        + '<input class='check_todo' name='check_todo' type='checkbox' />'
+                    +'</div>'
+                    +'<div class='todo_description'>'
+                        + todoDescription
+                    + '</div>'
+                + '</div>);
+        $('#todo_form')[0].reset();
+});
+                         
+$('.check_todo').unbind('click');
+$('.check_todo').click( function() {
+    var todo = $(this).parent().parent();
+    todo.toggleClass('checked');
+});   
+    
+return false;
