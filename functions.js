d38699ddceb72534c0b6254bb5a4d68c5b6ceb0b
Jamie Coombs <jamiec.coombs@gmail.com>
2013-10-05 09:36:58 -0600

    trying to write function for todoMVC, obviously a work in progress

---
+++ functions.js
@@ -0,0 +1,12 @@
+var listItems = function (text) {
+ var result = [];
+    for(var index = 0; index < text.length; index++) {
+
+        var = newData =
+        var completed = '';
+        var checked = '';
+        
+    }
+    document.write('<li>' + text[i] + '</li>');
+    return result;
+}
