5c510360a1eae1fe0e091d7741565a3f403a8d18
Jamie Coombs <jamiec.coombs@gmail.com>
2013-10-26 10:57:20 -0600

    took out some comments from class, no change in actual code

--- functions.js
+++ functions.js
@@ -9,21 +9,15 @@ var app = {
     $('.show-complete').click(app.showComplete);
   },
   addNewItem: function(ev) {
-    // when keypress is enter
     if(ev.which === 13) {
-      // get the new todo label
       var label = $('.new-todo').val();
-      // create new todo item and append
       var newItem = $('.templates .todo-item').clone();
-      // update the label
       newItem.find('label').text(label)
-      // append to list
       newItem.appendTo('.todo-list');
-      // reset todo input
       $('.new-todo').val('');
-      // update remaining
       app.updateRemaining();
-    }
+      }
+ 
   }, 
   toggleStatus: function(ev) {
     $(ev.target).closest('.todo-item').toggleClass('complete');
